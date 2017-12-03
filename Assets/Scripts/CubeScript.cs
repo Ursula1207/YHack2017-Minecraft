df44e19e487c5e3eecc13ebe0dc7960574f1be9d
Ursula Huang <ursulahuang@Ursulas-MacBook-Pro-2.local>
2017-12-03 04:17:39 -0500

    Added materials from Tanks! Tutorial; Cube generates Particle System when hit; Camera moves with WASD and cursor

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	public GameObject miningEffect;
+	public float health = 100f;
+	private bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+//		Debug.Log ("material:" + miningEffect.GetComponent<ParticleSystemRenderer> ().sharedMaterial);
+//		Debug.Log ("material2:" + GetComponent<MeshRenderer> ().material);
+//		ParticleSystemRenderer re = miningEffect.GetComponent<ParticleSystemRenderer> () ;
+//		re.renderMode = ParticleSystemRenderMode.Billboard;
+		miningEffect.GetComponent<ParticleSystemRenderer> ().sharedMaterial = gameObject.GetComponent<MeshRenderer> ().material;
+//		Debug.Log ("material3:" + miningEffect.GetComponent<ParticleSystemRenderer> ().sharedMaterial);
+//		miningEffect.GetComponent<ParticleSystemRenderer> ().renderMode = ParticleSystemRenderMode.Billboard;
+//		Debug.Log ("material4:" + miningEffect.GetComponent<ParticleSystemRenderer> ().sharedMaterial);
+
+	}
+
+	void Awake () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+		if (dead) {
+			gameObject.transform.Rotate (new Vector3 (15f, 30f, 45f) * Time.deltaTime);
+		}
+	}
+
+	void OnMouseUp() {
+		Debug.Log ("OnMouseUp");
+		GameObject effect = Instantiate (miningEffect);
+		effect.transform.position = transform.position;
+		Destroy (effect, 2f);
+		if (health <= 0f) {
+			gameObject.transform.localScale = new Vector3 (0.25f, 0.25f, 0.25f);
+			gameObject.transform.GetComponent<Rigidbody> ().velocity =
+				new Vector3 (Random.Range(-1f, 1f), Random.Range(0f, 2f), Random.Range(-1f, 1f));
+			dead = true;
+		} else {
+			health -= 25f;
+		}
+
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (dead) {
+			gameObject.transform.position += new Vector3 (0f, 0.15f, 0f);
+			gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			gameObject.GetComponent<Rigidbody> ().useGravity = false;
+		}
+	}
+
+	void OnMouseEnter() {
+		Camera.main.GetComponent<MovementScript> ().isRotating = false;
+	}
+
+	void OnMouseExit() {
+		Camera.main.GetComponent<MovementScript> ().isRotating = true;
+	}
+}
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+	public float speed = 2f;
+	public bool isRotating = true;
+	public float turnSpeed = 4.0f;
+	private float yaw = 0f;
+	private float pitch = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
+		}
+		if (isRotating) {
+			yaw += turnSpeed * Input.GetAxis ("Mouse X");
+			pitch -= turnSpeed * Input.GetAxis ("Mouse Y");
+
+			transform.eulerAngles = new Vector3 (pitch, yaw, 0.0f);
+		}
+	}
+}
